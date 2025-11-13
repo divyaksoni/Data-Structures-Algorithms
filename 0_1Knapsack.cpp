bf102f6ee679d693b6661fa75edc764ac4e42a48
divyaksoni <107862285+divyaksoni@users.noreply.github.com>
2025-11-13 23:44:28 +0530

    0_1Knapsack.cpp
    All three approaches (Recursive, Memoization & Tabulation) for 0/1 Knapsack

--- 0_1Knapsack.cpp
+++ 0_1Knapsack.cpp
@@ -12,7 +12,10 @@ int knapsack(int W, vector<int> &val, vector<int> &wt)
         //memoisation
         // vector<vector<int>>dp(n+1, vector<int>(W+1, -1));
         // return helper(val, wt, n, W, dp);
-        
+
+		// tabulation
+        // vector<vector<int>>dp(n+1, vector<int>(W+1));
+        // return helper(val, wt, n, W, dp);
     }
     
     // Recursion
@@ -67,6 +70,33 @@ int knapsack(int W, vector<int> &val, vector<int> &wt)
     //     }
         
     //     return dp[n][W]= max(profit1, profit2);
+    // }
+
+	// Tabulation
+    
+    // int helper(vector<int>& val, vector<int>& wt, int n, int W, vector<vector<int>>& dp)
+    // {
+    //     // Initialisation
+    //     for(int i=0; i<n+1;i++)
+    //         dp[i][0]=0;
+        
+    //     for(int j=0; j<W+1;j++)
+    //         dp[0][j]=0;  
+            
+    //     // Transition Function
+    //     for(int i=1;i<n+1;i++)
+    //     {
+    //         for(int j=1;j<W+1;j++)
+    //         {
+    //             if(j>=wt[i-1])
+    //             {
+    //                 dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+    //             }
+    //             else
+    //                 dp[i][j]= dp[i-1][j];
+    //         }
+    //     }
+    //     return dp[n][W];
     // }
 
 int main() 
